6ec60a0e8f0a83b6f510e7bcdebd32a133f3f8d5
Jack Maloney <jack@maloney.ai>
2023-09-19 16:52:46 -0500

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,94 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    ### Nixpkgs ###
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs { inherit system; };
-  in
-    {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          pkgs.elan
-        ];
+    ### Flake / Project Inputs ###
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    flake-root.url = "github:srid/flake-root";
+
+    mission-control.url = "github:Platonic-Systems/mission-control";
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.flake-utils.inputs.systems.follows = "systems";
+    };
+
+    systems.url = "github:nix-systems/default";
+
+    treefmt = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    flake-root,
+    mission-control,
+    pre-commit-hooks,
+    systems,
+    treefmt,
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} ({
+      withSystem,
+      inputs,
+      ...
+    }: {
+      systems = import systems;
+      imports = [
+        flake-root.flakeModule
+        mission-control.flakeModule
+        pre-commit-hooks.flakeModule
+        treefmt.flakeModule
+      ];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        lib,
+        ...
+      }: {
+        packages = {};
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.mission-control.devShell
+            config.pre-commit.devShell
+            config.treefmt.build.devShell
+          ];
+          buildInputs = with pkgs; [
+            elan
+          ];
+        };
+
+        mission-control.scripts = {
+          fmt = {
+            description = "Format the source tree";
+            exec = config.treefmt.build.wrapper;
+            category = "Dev Tools";
+          };
+        };
+
+        pre-commit = {
+          check.enable = true;
+          settings.hooks.treefmt.enable = true;
+          settings.settings.treefmt.package = config.treefmt.build.wrapper;
+        };
+
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+          programs.alejandra.enable = true;
+        };
+        formatter = config.treefmt.build.wrapper;
       };
     });
 }
